d1fd5628e57bd8288c07853e90a084bc6e6002af
Jan Miksovsky <jan@miksovsky.com>
2022-10-18 11:17:39 -0700

    Serve gist

---
+++ gist.js
@@ -0,0 +1,27 @@
+import {
+  ExplorableGraph,
+  graphVirtual,
+  MapValuesGraph,
+} from "@graphorigami/origami";
+import fetch from "node-fetch";
+
+export default async function gist(gistId) {
+  const gistUrl = `https://api.github.com/gists/${gistId}`;
+  const response = await fetch(gistUrl);
+  if (response.ok) {
+    let json;
+    try {
+      json = await response.json();
+    } catch (error) {
+      throw error;
+    }
+    const { files } = json;
+    // Top-level `files` has the actual file content in `content` properties.
+    const graph = new MapValuesGraph(files, (file) => file.get("content"));
+    const meta = await graphVirtual.call(this, graph);
+    const plain = await ExplorableGraph.plain(meta);
+    return meta;
+  } else {
+    return undefined;
+  }
+}
